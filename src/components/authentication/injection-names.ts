4d3b5a2eb9621882acde6e48048e95f7f1dda1fc
Thorsten Woywod <tw@web-computing.de>
2019-01-11 15:40:05 +0100

    remove component from authenticationInjectionNames

--- src/components/authentication/injection-names.ts
+++ src/components/authentication/injection-names.ts
@@ -1,10 +1,6 @@
 /** Names of injectionable services, leads to fewer typing errors for most important injections */
 export const authenticationInjectionNames = {
   /**
-   * Inject an instance of @type {Component<Configuration.Runtime>}
-   */
-  component: "meta:component//authentication",
-  /**
    * Inject an instance of @type {(strategyClass: StrategyClass) => any}
    */
   strategyFactory: "authentication:strategy-factory",
