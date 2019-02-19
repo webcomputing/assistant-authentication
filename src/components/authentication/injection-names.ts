d47636041435e3063ce4b4e153a758b72a3c1b48
Antonius Ostermann <ostermann.antonius@gmail.com>
2019-02-19 16:22:38 +0100

    references type in injection-names

--- src/components/authentication/injection-names.ts
+++ src/components/authentication/injection-names.ts
@@ -1,7 +1,7 @@
 /** Names of injectionable services, leads to fewer typing errors for most important injections */
 export const authenticationInjectionNames = {
   /**
-   * Inject an instance of @type {(strategyClass: StrategyClass) => any}
+   * Inject an instance of @type {StrategyFactory}
    */
   strategyFactory: "authentication:strategy-factory",
 };
